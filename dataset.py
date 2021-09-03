11e41cd30a8021d80ca89b0fc0daa2d247676097
adeshkin <adeshkin.vi@phystech.edu>
2021-09-03 17:29:59 +0300

    baseline

---
+++ dataset.py
@@ -0,0 +1,46 @@
+import cv2
+import torch
+
+
+class RoadSegment(torch.utils.data.Dataset):
+    def __init__(self, df, image_dir, transform=None):
+        self.df = df
+        self.img_dir = image_dir
+        self.image_ids = self.df['Image_ID'].tolist()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        img = cv2.imread(f'{self.img_dir}/{image_id}.tif', cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        label = self.df[self.df['Image_ID'] == image_id]['Target'].item()
+
+        if self.transform:
+            image = self.transform(image=img)["image"]
+
+        return image, label
+
+
+class RoadSegmentTest(torch.utils.data.Dataset):
+    def __init__(self, df, image_dir, transform=None):
+        self.df = df
+        self.img_dir = image_dir
+        self.transform = transform
+        self.image_ids = self.df['Image_ID'].tolist()
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        img = cv2.imread(f'{self.img_dir}/{image_id}.tif', cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        if self.transform:
+            image = self.transform(image=img)["image"]
+
+        return image
---
+++ main.py
@@ -0,0 +1,185 @@
+import yaml
+import os
+import copy
+import random
+import numpy as np
+import pandas as pd
+import wandb
+import torch
+import torchvision
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from sklearn.model_selection import train_test_split
+
+from dataset import RoadSegment, RoadSegmentTest
+
+
+def calculate_accuracy(output, target):
+    output = torch.sigmoid(output) >= 0.5
+    target = target == 1.0
+    return torch.true_divide((target == output).sum(dim=0), output.size(0)).item()
+
+
+class Runner:
+    def __init__(self, params):
+        self.params = params
+        train_transform = A.Compose([
+            A.HorizontalFlip(p=0.5),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+            A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            A.RandomBrightnessContrast(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ])
+
+        val_transform = A.Compose([
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2()
+        ])
+
+        df = pd.read_csv(params['train_filepath'])
+        test_df = pd.read_csv(params['test_filepath'])
+
+        image_ids = df['Image_ID'].tolist()
+        labels = df['Target'].tolist()
+
+        train_ids, valid_ids = train_test_split(image_ids, test_size=params['test_size'], random_state=42,
+                                                stratify=labels)
+
+        train_df = df[df['Image_ID'].isin(train_ids)]
+        val_df = df[df['Image_ID'].isin(valid_ids)]
+
+        dataset_train = RoadSegment(train_df, params['image_dir'], train_transform)
+        dataset_val = RoadSegment(val_df, params['image_dir'], val_transform)
+        dataset_test = RoadSegmentTest(test_df, params['image_dir'], val_transform)
+
+        self.data_loaders = {'train': DataLoader(dataset_train,
+                                                 batch_size=params['batch_size'],
+                                                 shuffle=True,
+                                                 num_workers=params['batch_size']),
+
+                             'val': DataLoader(dataset_val,
+                                               batch_size=params['batch_size'],
+                                               shuffle=False,
+                                               num_workers=params['batch_size']),
+
+                             'test': DataLoader(dataset_test,
+                                                batch_size=1,
+                                                shuffle=False,
+                                                num_workers=1)}
+
+        self.model = torchvision.models.resnet50(pretrained=True, num_classes=1)
+        self.device = torch.device(params['device'])
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=params["lr"])
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.checkpoints_dir = params['checkpoint_dir']
+        self.submissions_dir = params['submission_dir']
+
+    def train(self):
+        self.model.train()
+
+        epoch_metrics = dict()
+        epoch_metrics['loss'] = 0.0
+        epoch_metrics['acc'] = 0.0
+        for images, labels in tqdm(self.data_loaders['train']):
+            images = images.to(self.device)
+            labels = labels.to(self.device).float().view(-1, 1)
+
+            pred_labels = self.model(images)
+            loss = self.model(pred_labels, labels)
+            accuracy = calculate_accuracy(pred_labels, labels)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            epoch_metrics['loss'] += loss.cpu().detach()
+            epoch_metrics['acc'] += accuracy.cpu().detach()
+
+        for m in epoch_metrics:
+            epoch_metrics[m] = epoch_metrics[m] / len(self.data_loaders['train'])
+
+        return epoch_metrics
+
+    def eval(self):
+        epoch_metrics = dict()
+        epoch_metrics['loss'] = 0.0
+        epoch_metrics['acc'] = 0.0
+        self.model.eval()
+        with torch.no_grad():
+            for images, labels in tqdm(self.data_loaders['val']):
+                images = images.to(self.device)
+                labels = labels.to(self.device).float().view(-1, 1)
+
+                pred_labels = self.model(images)
+                loss = self.model(pred_labels, labels)
+                accuracy = calculate_accuracy(pred_labels, labels)
+
+                epoch_metrics['loss'] += loss.cpu().detach()
+                epoch_metrics['acc'] += accuracy.cpu().detach()
+
+        for m in epoch_metrics:
+            epoch_metrics[m] = epoch_metrics[m] / len(self.data_loaders['val'])
+
+        return epoch_metrics
+
+    def predict(self):
+        PATH = f"{self.checkpoints_dir}/{self.params['model_filename']}.pth"
+        self.model.load_state_dict(torch.load(PATH))
+        self.model.to(self.device)
+        self.model.eval()
+        results = []
+        with torch.no_grad():
+            for images, image_ids in tqdm(self.data_loaders['test']):
+                pred_labels = self.model(images)
+                for image_id, pred_label in zip(image_ids, pred_labels):
+                    row_dict = {}
+                    row_dict["Image_ID"] = image_id
+                    row_dict["Target"] = pred_label
+                    results.append(row_dict)
+
+        df = pd.DataFrame(results)
+        df.to_csv(f"{self.submissions_dir}/{self.params['submission_filename']}.csv", index=False)
+
+    def run(self):
+        random.seed(42)
+        np.random.seed(42)
+
+        wandb.init(project=self.params['project_name'], config=self.params)
+
+        os.makedirs(self.checkpoints_dir, exist_ok=True)
+        os.makedirs(self.submissions_dir, exist_ok=True)
+
+        best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_acc = 0.
+
+        self.model = self.model.to(self.device)
+        for epoch in range(params['num_epochs']):
+            train_metrics = self.train()
+            val_metrics = self.eval()
+
+            logs = {'train': train_metrics,
+                    'val': val_metrics}
+
+            wandb.log(logs, step=epoch)
+
+            current_val_acc = val_metrics['acc']
+            if current_val_acc > best_acc:
+                best_acc = current_val_acc
+                best_model_wts = copy.deepcopy(self.model.state_dict())
+
+        self.model.load_state_dict(best_model_wts)
+        torch.save(self.model.state_dict(), f"{self.checkpoints_dir}/{self.params['model_filename']}.pth")
+
+
+if __name__ == '__main__':
+    with open('./configs/default.yaml', 'r') as file:
+        params = yaml.load(file, yaml.Loader)
+
+    runner = Runner(params)
+    runner.run()
+    # runner.predict()
