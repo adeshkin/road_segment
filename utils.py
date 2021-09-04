fcbc3640f580626c6b48806adee270236bf7eaea
adeshkin <adeshkin.vi@phystech.edu>
2021-09-04 12:15:52 +0300

    zany-donkey-20

--- utils.py
+++ utils.py
@@ -27,7 +27,7 @@ def get_transform():
     train_transform = A.Compose([
         A.HorizontalFlip(p=0.5),
         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=90, p=0.5),
-        A.CoarseDropout(min_height=32, max_height=48, min_width=32, max_width=48, p=0.5),
+        A.CoarseDropout(min_height=32, max_height=64, min_width=32, max_width=64, p=0.5),
         A.GaussNoise(var_limit=(10, 50), p=0.5),
         A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
