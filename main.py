b0ccb8901c6496a50e38b521a57e39d5f9c9c3e2
adeshkin <adeshkin.vi@phystech.edu>
2021-09-05 20:41:34 +0300

    ensemble_5x_efficientnet-b2_lr_sched_aug_

--- main.py
+++ main.py
@@ -168,8 +168,8 @@ class Runner:
 
     def predict_ensemble(self):
         models = []
-        for arch, path in zip(['efficientnet-b1', 'efficientnet-b1', 'efficientnet-b1', 'efficientnet-b1', 'efficientnet-b1'],
-                              ['honest-violet-5', 'driven-glade-7', 'visionary-dawn-9', 'icy-oath-11', 'serene-fire-13']):
+        for arch, path in zip(['efficientnet-b2', 'efficientnet-b2', 'efficientnet-b2', 'efficientnet-b2', 'efficientnet-b2'],
+                              ['glamorous-plant-6', 'pleasant-bush-8', 'soft-breeze-10', 'comic-pyramid-12', 'quiet-blaze-14']):
             if 'resnet' in arch:
                 model = torchvision.models.__dict__[arch]()
                 model.fc = nn.Linear(model.fc.in_features, 1)
@@ -211,7 +211,7 @@ class Runner:
                 results.append(row_dict)
 
         df = pd.DataFrame(results)
-        df.to_csv(f"{self.submissions_dir}/ensemble_5x_efficientnet-b1_lr_sched_aug_{self.params['ensemble_mode']}.csv", index=False)
+        df.to_csv(f"{self.submissions_dir}/ensemble_5x_efficientnet-b2_lr_sched_aug_{self.params['ensemble_mode']}.csv", index=False)
 
     def run(self):
         random.seed(42)
